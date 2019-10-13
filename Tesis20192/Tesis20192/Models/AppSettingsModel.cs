472dfb6690566f459e10f9a1cf59ab105a4dd05f
Luis Mallma <luis_mp2019@outlook.com>
2019-10-12 22:16:14 -0500

    pull2

---
+++ Tesis20192/Tesis20192/Models/AppSettingsModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tesis20192.Models
+{
+    public class AppSettingsModel
+    {
+    }
+}
---
+++ Tesis20192/Tesis20192/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Tesis20192.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
