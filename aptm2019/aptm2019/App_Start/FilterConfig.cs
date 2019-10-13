3fbaf38752fb05313075fcfcbe821c619394d73a
Luis Mallma <luis_mp2019@outlook.com>
2019-10-13 00:48:22 -0500

    insert

---
+++ aptm2019/aptm2019/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace aptm2019
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
